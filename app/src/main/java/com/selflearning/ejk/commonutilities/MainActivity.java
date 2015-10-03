a7817ddfe4e44d4eb2bfcd0d855a5f1ffe99982e
Erik-Jan Krielen <ej-krielen@gmail.com>
2015-10-03 22:08:23 +0200

    Made ready for testing

--- app/src/main/java/com/selflearning/ejk/commonutilities/MainActivity.java
+++ app/src/main/java/com/selflearning/ejk/commonutilities/MainActivity.java
@@ -5,11 +5,14 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +51,12 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //TODO test code here
+        Log.d(TAG, "test");
     }
 }
