fdad530f89646151cba82ed3552a1ee7081d617d
Erik-Jan Krielen <ej-krielen@gmail.com>
2015-10-03 20:27:44 +0200

    Added InternetConnection, added method to check for network connection

---
+++ app/src/main/java/com/selflearning/ejk/commonutilities/online/InternetConnection.java
@@ -0,0 +1,27 @@
+package com.selflearning.ejk.commonutilities.online;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Methods to check for internet connection
+ *
+ * @author Erik-Jan Krielen ej.krielen@gmail.com
+ * @since 3-10-2015
+ */
+public class InternetConnection {
+
+    /**
+     * @return Check to see if the device has an internet network connection (true/false)
+     */
+    public static boolean checkNetworkConnection(Activity activity) {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) activity.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+    }
+
+
+}
