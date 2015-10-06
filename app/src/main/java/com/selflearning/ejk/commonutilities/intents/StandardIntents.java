23575c45e7e96212276d6acae9272611c3cd2148
Erik-Jan Krielen <ej-krielen@gmail.com>
2015-10-06 22:42:06 +0200

    Added twitter intents

--- app/src/main/java/com/selflearning/ejk/commonutilities/intents/StandardIntents.java
+++ app/src/main/java/com/selflearning/ejk/commonutilities/intents/StandardIntents.java
@@ -35,16 +35,62 @@ public final class StandardIntents {
     /**
      * Goes to the device default call number activity with the number filled in
      *
-     * @param context context from where the method was called
+     * @param context      context from where the method was called
      * @param emailAddress email address to send email to
-     * @param subject subject line of the email
+     * @param subject      subject line of the email
      */
-    private static  void startEmailActivity(Context context, String emailAddress, String subject) {
+    public static void startEmailActivity(Context context, String emailAddress, String subject) {
         if (!TextUtils.isEmpty(emailAddress) && !TextUtils.isEmpty(subject)) {
             Intent intent = new Intent(Intent.ACTION_SENDTO);
             intent.setData(Uri.parse("mailto:")); // only email apps should handle this
             intent.putExtra(Intent.EXTRA_EMAIL, emailAddress);
             intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+            if (intent.resolveActivity(context.getPackageManager()) != null) {
+                context.startActivity(intent);
+            }
+        } else {
+            Toast.makeText(context, context.getString(R.string.error), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Starts an activity to post a tweet, both parameters are optional
+     *
+     * @param context  calling context
+     * @param message  optional: standard message to send
+     * @param hashtags optional: hashtag(s) to add (comma seperated)
+     */
+    public static void startPostToTwitterActivity(Context context, String message, String hashtags) {
+
+        String twitterString = "https://twitter.com/intent/tweet?";
+
+        if (!TextUtils.isEmpty(message)) {
+            twitterString += "text=" + message;
+        }
+        if (!TextUtils.isEmpty(hashtags)) {
+            twitterString += "&hashtags=" + hashtags;
+        }
+        Uri twitterUri = Uri.parse(twitterString);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW, twitterUri);
+        intent.putExtra(Intent.EXTRA_TEXT, "message");
+        if (intent.resolveActivity(context.getPackageManager()) != null) {
+            context.startActivity(intent);
+        }
+
+    }
+
+    /**
+     * Starts an activity displaying a twitter account
+     *
+     * @param context     calling context
+     * @param accountName name of twitter account to go to
+     */
+    public static void startGoToTwitterActivity(Context context, String accountName) {
+        if (!TextUtils.isEmpty(accountName)) {
+            String twitterUrl = "twitter://user?screen_name=";
+            Uri webpage = Uri.parse(twitterUrl + accountName);
+            Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
             if (intent.resolveActivity(context.getPackageManager()) != null) {
                 context.startActivity(intent);
             }
