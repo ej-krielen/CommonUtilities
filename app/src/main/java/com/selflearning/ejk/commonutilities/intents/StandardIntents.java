b40a346235e12611b1085fb5e58b5b8f0e345ac2
Erik-Jan Krielen <ej-krielen@gmail.com>
2015-10-06 21:48:20 +0200

    Refactor to do a null check

--- app/src/main/java/com/selflearning/ejk/commonutilities/intents/StandardIntents.java
+++ app/src/main/java/com/selflearning/ejk/commonutilities/intents/StandardIntents.java
@@ -3,6 +3,10 @@ package com.selflearning.ejk.commonutilities.intents;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.text.TextUtils;
+import android.widget.Toast;
+
+import com.selflearning.ejk.commonutilities.R;
 
 /**
  * Standard intents
@@ -16,12 +20,16 @@ public final class StandardIntents {
      * Goes to the phone's default call number screen with the number filled in
      *
      * @param context context from where the method was callded
-     * @param number telephone number to call
+     * @param number  telephone number to call
      */
     public static void startPhoneActivity(Context context, String number) {
-        String url = "tel:" + number.trim();
-        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
-        context.startActivity(intent);
+        if (!TextUtils.isEmpty(number)) {
+            String url = "tel:" + number.trim();
+            Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
+            context.startActivity(intent);
+        } else {
+            Toast.makeText(context, context.getString(R.string.error), Toast.LENGTH_SHORT).show();
+        }
     }
 
 }
