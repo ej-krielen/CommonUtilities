b6d0974d416a12c1da1c6aae63dff8d1533847cb
Erik-Jan Krielen <ej-krielen@gmail.com>
2015-10-06 22:55:59 +0200

    Added method for progress dialog

--- app/src/main/java/com/selflearning/ejk/commonutilities/ui/dialogs/Dialogs.java
+++ app/src/main/java/com/selflearning/ejk/commonutilities/ui/dialogs/Dialogs.java
@@ -1,6 +1,8 @@
 package com.selflearning.ejk.commonutilities.ui.dialogs;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 
@@ -19,11 +21,12 @@ public final class Dialogs {
      * @param activity          calling activity
      * @param title             Title of the dialog
      * @param message           Message of the dialog
-     * @param positiveButton    String for positiveButton
      * @param hasPositiveButton pass false if you don't want a button at all
+     * @param positiveButton    String for positiveButton
      * @param canCancel         pass false if you want to force the user to use the button(s) to dismiss the dialog
+     * @return AlertDialog
      */
-    public static void simpleDialog(Activity activity, String title, String message, String positiveButton, boolean hasPositiveButton, boolean canCancel) {
+    public static AlertDialog simpleDialog(Activity activity, String title, String message, boolean hasPositiveButton, String positiveButton, boolean canCancel) {
         AlertDialog.Builder builder = new AlertDialog.Builder(activity);
         builder.setTitle(title)
                 .setMessage(message);
@@ -44,5 +47,24 @@ public final class Dialogs {
 
         AlertDialog dialog = builder.create();
         dialog.show();
+        return dialog;
+    }
+
+    /**
+     * Simple Progress Dialog. Don't forget to dismiss (in onPause).
+     *
+     * @param context calling context
+     * @param title   title for dialog
+     * @param message message for dialog
+     * @return ProgressDialog
+     */
+    public static ProgressDialog showProgress(Context context, String title, String message) {
+        ProgressDialog progressDialog = new ProgressDialog(context);
+        progressDialog.setTitle(title);
+        progressDialog.setMessage(message);
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+
+        return progressDialog;
     }
 }
