08e8b1adb12076176b4ef7a5fa854ee6c91416a9
Erik-Jan Krielen <ej-krielen@gmail.com>
2015-10-06 21:39:37 +0200

    Added standard intent class

---
+++ app/src/main/java/com/selflearning/ejk/commonutilities/intents/StandardIntents.java
@@ -0,0 +1,11 @@
+package com.selflearning.ejk.commonutilities.intents;
+
+/**
+ * Standard intents
+ *
+ * @author Erik-Jan Krielen ej.krielen@gmail.com
+ * @since 6-10-2015
+ */
+public final class StandardIntents {
+
+}
