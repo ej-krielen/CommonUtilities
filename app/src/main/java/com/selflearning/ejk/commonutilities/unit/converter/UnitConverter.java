367be3be93cddaed2b9b18a1a95a1a4e5be87a37
Erik-Jan Krielen <ej-krielen@gmail.com>
2015-10-06 23:13:01 +0200

    Added UnitConverter class, added method to convert celsius to fahrenheit, added method fahrenheit to celsius

---
+++ app/src/main/java/com/selflearning/ejk/commonutilities/unit/converter/UnitConverter.java
@@ -0,0 +1,19 @@
+package com.selflearning.ejk.commonutilities.unit.converter;
+
+/**
+ * Methods to convert units
+ *
+ * @author Erik-Jan Krielen ej.krielen@gmail.com
+ * @since 6-10-2015
+ */
+public final class UnitConverter {
+
+    public static double celsiusToFahrenheit(double celsius) {
+        return (celsius * 1.8 + 32);
+    }
+
+    public static double fahrenheitToCelcius(double fahrenheit) {
+        return ((fahrenheit - 32) / 1.8);
+    }
+
+}
