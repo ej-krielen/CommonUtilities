b92ca40cbd66882af03d960c8c9d1f0c76b118b1
Erik-Jan Krielen <ej-krielen@gmail.com>
2015-10-06 21:39:57 +0200

    Added phone number method

--- app/src/main/java/com/selflearning/ejk/commonutilities/intents/StandardIntents.java
+++ app/src/main/java/com/selflearning/ejk/commonutilities/intents/StandardIntents.java
@@ -1,5 +1,9 @@
 package com.selflearning.ejk.commonutilities.intents;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+
 /**
  * Standard intents
  *
@@ -7,5 +11,17 @@ package com.selflearning.ejk.commonutilities.intents;
  * @since 6-10-2015
  */
 public final class StandardIntents {
+
+    /**
+     * Goes to the phone's default call number screen with the number filled in
+     *
+     * @param context context from where the method was callded
+     * @param number telephone number to call
+     */
+    public static void startPhoneActivity(Context context, String number) {
+        String url = "tel:" + number.trim();
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse(url));
+        context.startActivity(intent);
+    }
 
 }
